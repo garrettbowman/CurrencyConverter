fe3dfe4a94f2a3224031e1d308803093f7719860
Gary Bowman <garrettbowman96@gmail.com>
2024-02-01 10:01:38 -0500

    Add additional currencies for conversion

--- currency.py
+++ currency.py
@@ -6,7 +6,7 @@ API_KEY = "fca_live_xfI5TZyCCExiqgYc9CNISPTiAHPbHp5UCavdCXKz"
 
 BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
 
-CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY"]
+CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY", "JPY", "INR", "GBP", "NZD", "CHF"]
 
 def convert_currency(base):
     currencies = ",".join(CURRENCIES)
@@ -14,10 +14,18 @@ def convert_currency(base):
     try:
         response = requests.get(url)
         data = response.json()
-        print(data)
-        return data
+        return data["data"]
     except Exception as e:
         print(e)
         return None
     
-convert_currency("USD")
+
+base = input("Enter the base currency (q to quit): ").upper()
+
+#if base not in CURRENCIES:
+    #print("Invalid currency")
+
+data = convert_currency(base)
+del data[base]
+for ticker, value in data.items():
+    print(f"{ticker}: {value}")
